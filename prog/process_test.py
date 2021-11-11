07fe92b822d33923a39d671f3107ac4c66142604
fmkr-project <91734800+fmkr-project@users.noreply.github.com>
2021-11-11 19:42:21 +0100

    Add files via upload

--- prog/process_test.py
+++ prog/process_test.py
@@ -7,6 +7,9 @@ from math import atan2
 cur_im = io.imread("no_kiha.png")
 imem = deepcopy(cur_im)
 
+
+    
+
 def render():
     "affiche l'image en cours de traitement avec plt"
     pl.imshow(cur_im)
@@ -25,26 +28,56 @@ def grayscale(im):
             hl = 0.227*coord[0] + 0.587*coord[1] + 0.114*coord[2]
             coord[:3] = hl
 
-gaussian3 = [[1/9 for i in range(3)] for i in range(3)]
+def new_mean(im,var,thr = 128):
+    "modifie la luminosité de l'image"
+    for li in range(len(im[0])-1):
+        for col in range(len(im)-1):
+            coord = im[col][li]
+            if coord[0] > thr:
+                is_bright = 1
+            else:
+                is_bright = -1
+            coord[:3] = coord[:3] + var*(is_bright)
+    
 
-sob_v = [[1/4,0,-1/4],[1/2,0,-1/2],[1/4,0,-1/4]]
-sob_h = [[1/4,1/2,1/4],[0,0,0],[-1/4,-1/2,-1/4]]
+grayscale(cur_im) # on travaille avec une image en NB pour la suite
+
+gaussian3 = [[1/16,1/8,1/16],[1/8,1/4,1/8],[1/16,1/8,1/16]]
+
+
+# Filtres de SOBEL
+#sob_v = [[1/4,0,-1/4],[1/2,0,-1/2],[1/4,0,-1/4]]
+#sob_h = [[1/4,1/2,1/4],[0,0,0],[-1/4,-1/2,-1/4]]
+sob_v = [[-1,0,1],[-2,0,2],[-1,0,1]]
+sob_h = [[1,2,1],[0,0,0],[-1,-2,-1]]
+
+# Filtres de PREWITT
 prew_h = [[1/3,0,-1/3],[1/3,0,-1/3],[1/3,0,-1/3]]
 prew_v = [[1/3,1/3,1/3],[0,0,0],[-1/3,-1/3,-1/3]]
+
+# Autres
 pb1 = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
+sharp = [[0,-1,0],[-1,5,-1],[0,-1,0]]
+
+
 
 def conv(im,mat):
     "applique une matrice de convolution à une image"
+    imbis = deepcopy(im) # Copie pour préserver les données
     center = len(mat) // 2
     size = center
     for li in range(len(im[0])-1):
         for col in range(len(im)-1):
             coord = im[col][li]
             coef = coord[0]
+            mat_flat = [row[k] for row in mat for k in range(len(row))]
+            # on aplatit la matrice de convolution
+            prox = [imbis[j+col][i+li][0]\
+                    for i in range(-center, center+1)\
+                    for j in range(-center, center+1)]
+            hl = sum([prox[i]*mat_flat[i] for i in range(len(prox))])
+            #print(prox)
             #try:
-            hl = sum([im[j+col][i+li][0]*mat[i+center][j+center]\
-                        for i in range(-center, center+1)\
-                        for j in range(-center, center+1)])
             #except:
             #    hl = 0
             #if hl < 0:
@@ -55,18 +88,17 @@ def conv(im,mat):
             im[col][li][:3] = int(hl)
 
 
-def prew_cont(im):
-    "technique de détection des contours en utilisant les filtres de PREWITT"
-    grayscale(im)
-    conv(im,gaussian3)
+def sobcontour(im):
+    "technique de détection des contours en utilisant les filtres de SOBEL"
+    #conv(im,gaussian3)
     #conv(im,gaussian3) -> floutage plus poussé ?
     
     imv = deepcopy(im) # Copie de l'image pour les filtres de dérivation
     imh = deepcopy(im)
     direc = [[None for col in range(len(im[0])-1)]for li in range(len(im)-1)]
     # Création d'un tableau vide de même taille que l'image
-    conv(imh,prew_h)
-    conv(imv,prew_v)
+    conv(imh,sob_h)
+    conv(imv,sob_v)
     
     
     for li in range(len(im[0])-1):
@@ -75,23 +107,23 @@ def prew_cont(im):
             dx = imv[col][li][0]
             dy = imh[col][li][0]
             coord[:3] = (dx**2 + dy**2)**0.5
-            direc[col][li] = int((atan2(dy,dx)/(2*np.pi))*360)
-    print(direc)
+            #direc[col][li] = int((atan2(dy,dx)/(2*np.pi))*360)
+    
     
 
 
 
-##def duochrome(im):
-##    "transforme une image nuance de gris en image NB"
-##    for li in range(len(im[0])-1):
-##        for col in range(len(im)-1):
-##            coord = im[col][li]
-##            if coord[0] != coord[1] and coord[0] != coord[2]:
-##                coord[:3] = 0
-##            elif coord[0] < 128:
-##                coord[:3] = 0
-##            else:
-##                coord[:3] = 255
+def duochrome(im):
+    "transforme une image nuance de gris en image NB"
+    for li in range(len(im[0])-1):
+        for col in range(len(im)-1):
+            coord = im[col][li]
+            if coord[0] != coord[1] and coord[0] != coord[2]:
+                coord[:3] = 0
+            elif coord[0] < 128:
+                coord[:3] = 0
+            else:
+                coord[:3] = 255
 
 
 def lowbound(im,thr=50):
